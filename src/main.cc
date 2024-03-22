55550593155c74253d9067a27b881b6353edd061
Kapil Sharma <kapilsharma.frm@gmail.com>
2024-03-22 10:47:11 -0400

    Added cuda-learn basic cmake setup

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "gpu.hpp"
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    std::cout << "CUDA: On" << std::endl;
+    printCudaVersion();
+
+    return 0;
+}
