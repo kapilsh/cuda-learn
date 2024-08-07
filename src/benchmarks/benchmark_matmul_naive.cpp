9e8e967d37a41febcc946eaa936624a9da4fc752
Kapil Sharma <kapilsharma.frm@gmail.com>
2024-08-07 11:57:29 -0400

    fix benchmark

--- src/benchmarks/benchmark_matmul_naive.cpp
+++ src/benchmarks/benchmark_matmul_naive.cpp
@@ -10,20 +10,19 @@ int main() {
         cudaEventCreate(&start);
         cudaEventCreate(&stop);
         std::vector<int64_t> sizes = {size, size};
+        cudaEventRecord(start);
         const auto x = torch::randn(sizes, torch::kCUDA);
         const auto y = torch::randn(sizes, torch::kCUDA);
-        // std::cout << "dtype" << x.dtype() << "\n";
-        
-        cudaEventRecord(start);
-        auto output = matmul_naive(x, y);
+
+        // auto output = matmul_naive(x, y);
+        auto output = torch::mm(x, y);
         cudaEventRecord(stop);
         cudaEventSynchronize(stop);
         float milliseconds = 0;
         cudaEventElapsedTime(&milliseconds, start, stop);
 
         const auto size_casted = static_cast<double>(size);
-        const auto tflops = 2 * size_casted* size_casted * size_casted * 10e-12 / (milliseconds * .0001);
+        const auto tflops = (2.0 * size_casted * size_casted * size_casted) / (milliseconds * 1e9);
         std::cout << "[" << size << "x" << size << "] Time taken: "  << milliseconds << "ms | " "TFLOPS: " << tflops  << "\n";
     }
-    return 0;
 }
